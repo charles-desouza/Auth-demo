699db9409aef8841bffcca67685339d86ec48bad
charles-desouza <charles5388@gmail.com>
2019-03-21 11:33:31 +0530

    Initial commit

---
+++ HttpBasicAuth/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HttpBasicAuth.Middleware
+{
+  public static class MiddlewareExtensions
+  {
+    public static IApplicationBuilder UseBasicAuthentication(this IApplicationBuilder builder)
+    {
+      return builder.UseMiddleware<BasicAuthMiddleware>("Secured Pages");
+    }
+  }
+}
