390769ae08d13d7f38f3aeb647abf2ebac9cc83f
charles-desouza <charles5388@gmail.com>
2019-07-09 14:16:12 +0530

    checked in files

---
+++ JwtAuth/Data/AppIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using JwtAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuth.Data
+{
+  public class AppIdentityDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public AppIdentityDbContext(DbContextOptions options) : base(options)
+    {
+    }
+  }
+}
