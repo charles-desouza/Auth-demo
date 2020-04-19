9aae2b8e94c6c3d43c26df0b97a39d11a379189b
charles-desouza <charles5388@gmail.com>
2020-04-19 16:27:49 +0530

    added Custom user data

--- IdentityAuth/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IdentityAuth/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using IdentityAuth.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -46,6 +47,16 @@ namespace IdentityAuth.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Full name")]
+            public string Name { get; set; }
+
+            [Required]
+            [Display(Name = "Birth Date")]
+            [DataType(DataType.Date)]
+            public DateTime DOB { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -74,7 +85,13 @@ namespace IdentityAuth.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { 
+                            UserName = Input.Email, 
+                            Name = Input.Name,
+                            DOB = Input.DOB,
+                            Email = Input.Email 
+                            };
+                            
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
