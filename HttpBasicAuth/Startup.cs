d418f45cf1244eb33c9123d654621861c1bd5570
charles-desouza <charles5388@gmail.com>
2020-04-22 22:59:47 +0530

    fixed routing issue

--- HttpBasicAuth/Startup.cs
+++ HttpBasicAuth/Startup.cs
@@ -37,8 +37,8 @@ namespace HttpBasicAuth
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
-
-      app.UseHttpsRedirection();
+      app.UseRouting();
+      //app.UseHttpsRedirection();
       
       app.UseEndpoints(endpoints =>
       {
