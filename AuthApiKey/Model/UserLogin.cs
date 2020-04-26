b9f030a11a43409e9997d20357412af5e613e5e2
charles-desouza <charles5388@gmail.com>
2020-04-26 16:00:49 +0530

    Added login fields

---
+++ AuthApiKey/Model/UserLogin.cs
@@ -0,0 +1,15 @@
+namespace AuthApiKey.Model
+{
+    public class UserLogin
+    {
+        public int Id { get; set; }
+
+        public string ApiKey { get; set; }
+        public string Owner { get; set; }
+        public bool Disabled { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime LastUsed { get; set; }
+        
+    }
+}
