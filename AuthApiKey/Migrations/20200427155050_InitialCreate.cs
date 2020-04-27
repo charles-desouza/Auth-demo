82f2e06b309f9f6c20c506ae84d40dbf00e3588c
charles-desouza <charles5388@gmail.com>
2020-04-27 21:21:36 +0530

    added migrations

---
+++ AuthApiKey/Migrations/20200427155050_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthApiKey.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLogins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ApiKey = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true),
+                    Disabled = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    LastUsed = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLogins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    UserLoginId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_UserLogins_UserLoginId",
+                        column: x => x.UserLoginId,
+                        principalTable: "UserLogins",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_UserLoginId",
+                table: "UserRoles",
+                column: "UserLoginId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropTable(
+                name: "UserLogins");
+        }
+    }
+}
